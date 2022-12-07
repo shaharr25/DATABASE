2163726cb3ecd884aeb6851ac99f89d3a9842098
shaharr25 <114142904+shaharr25@users.noreply.github.com>
2022-12-07 15:51:48 +0200

    Update syncdb.py

--- syncdb.py
+++ syncdb.py
@@ -1,10 +1,16 @@
 import threading
 import multiprocessing
 from filedb import Filedb
+import logging
 
 
 class Syncdb():
     def __init__(self, database: Filedb, thread_or_process):
+        """
+        initializer
+        :param database: dictionary
+        :param thread_or_process: true if thread and false if proces
+        """
         if not isinstance(database, Filedb):
             raise ValueError("not filedb instance")
         self.database = database
@@ -16,30 +22,49 @@ class Syncdb():
             self.lock = multiprocessing.Lock()
 
     def get_value(self, key):
+        """
+        let only 10 readers read at the same time
+        :param: key the key of the dictionary
+        :return: the value of the key
+        """
         self.semaphore.acquire()
+        logging.debug("reader in")
         value = self.database.get_value(key)
         self.semaphore.release()
+        logging.debug("reader out")
         return value
 
     def set_value(self, key, val):
+        """
+        let only one user set a new value in the dictionary with no other readers or writers at the same time
+        :param: key the key of the dictionary
+        :param: val value to put in the dictionary
+        :return: True if the value added to dictionary and false if not
+        """
         self.lock.acquire()
         for i in range(10):
             self.semaphore.acquire()
+        logging.debug("writer in")
         flag = self.database.set_value(key, val)
-        self.lock.release()
         for i in range(10):
             self.semaphore.release()
+        logging.debug("writer out")
+        self.lock.release()
         return flag
 
     def delete_value(self, key):
+        """
+        let only one user to delete from the dictionary with no other readers or writers at the same time
+        :param key: the key of the dictionary
+        :return: the value that was deleted
+        """
         self.lock.acquire()
         for i in range(10):
             self.semaphore.acquire()
         flag = self.database.delete_value(key)
-        self.lock.release()
         for i in range(10):
             self.semaphore.release()
+        self.lock.release()
         return flag
 
 
-Syncdb()
