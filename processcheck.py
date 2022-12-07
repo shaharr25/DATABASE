0ecd04f5178b3876911d28b3ca05b818509a9bbc
shaharr25 <114142904+shaharr25@users.noreply.github.com>
2022-12-07 15:52:47 +0200

    Add files via upload

---
+++ processcheck.py
@@ -0,0 +1,59 @@
+from syncdb import Syncdb
+from filedb import Filedb
+from multiprocessing import Process
+import logging
+
+
+def writer(db, id):
+    """
+    writer gets access to write to the dictionary
+    :param db: dictionary
+    :return:  None
+    """
+    logging.debug("writer " + str(id) + " joined")
+    for i in range(100):
+        assert db.set_value(i, i)
+    for i in range(100):
+        flag = db.delete_value(i) == i or db.delete_value(i) is None
+        assert flag
+    logging.debug("writer " + str(id) + " left")
+
+
+def reader(db):
+    """
+    reader gets access to read value from the dictionary
+    :param db: dictionary
+    :return: None
+    """
+    logging.debug("reader joined")
+    for i in range(100):
+        flag = db.get_value(i) == i or db.get_value(i) is None
+        assert flag
+    logging.debug("reader left")
+
+
+def main():
+    logging.basicConfig(filename='logprocess.txt', level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(threadName)s %(message)s')
+    db = Syncdb(Filedb(), False)
+    logging.debug("no competition")
+    writer(db, 0)
+    reader(db)
+    logging.debug("in competition")
+    all_processes = []
+    i = 1
+    for i in range(0, 10):
+        process1 = Process(target=writer, args=(db, i))
+        i += 1
+        all_processes.append(process1)
+        process1.start()
+    for i in range(0, 50):
+        process1 = Process(target=reader, args=(db, ))
+        all_processes.append(process1)
+        process1.start()
+    for i in all_processes:
+        i.join()
+
+
+if __name__ == "__main__":
+    main()
