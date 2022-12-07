fd375b1c36cc232e8d502e9a1454ad5da84052e0
shaharr25 <114142904+shaharr25@users.noreply.github.com>
2022-12-07 15:51:24 +0200

    Update filedb.py

--- filedb.py
+++ filedb.py
@@ -10,17 +10,31 @@ class Filedb(Database):
         super().__init__()
 
     def dump(self):
+        """
+        writes the dictionary in the file
+        :return: None
+        """
         with open("dbfile.txt", "wb") as file:
             pickle.dump(self.dict, file)
         # pickling the dict and writing it in a file
 
     def load(self):
+        """
+        gets the file into the dictionary
+        :return: None
+        """
         with open("dbfile.txt", "rb") as file:
-            self.dict = pickle.load(file)
+            try:
+                self.dict = pickle.load(file)
+            except:
+                print("error")
 
     def set_value(self, key, val):
         """
-        override
+        sets a new value in the dictionary file
+        :param: key the key of the dictionary
+        :param: val value to put in the dictionary
+        :return: True if the value added to dictionary and false if not
         """
         self.load()
         flag = super().set_value(key, val)
@@ -28,15 +42,21 @@ class Filedb(Database):
         return flag
 
     def get_value(self, key):
+        """
+        returns the value from the dictionary file
+        :param: key the key of the dictionary
+        :return: the value of the key
+        """
         self.load()
         return super().get_value(key)
 
-    def delete_value(self,key):
+    def delete_value(self, key):
+        """
+        deletes the value of the key from the dictionary file
+        :param key: the key of the dictionary
+        :return: the value that was deleted
+        """
         self.load()
         val = super().delete_value(key)
         self.dump()
         return val
-
-
-db = Filedb()
-db.dump()
